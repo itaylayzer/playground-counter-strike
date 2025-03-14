6441cf39b979d5f667fe9c3bd5574047ba73ad89
itaylayzer <itaylayzer@gmail.com>
2025-03-14 18:56:22 +0000

    Notes

--- src/app/Index.tsx
+++ src/app/Index.tsx
@@ -13,6 +13,22 @@ function App() {
 					<GoDotFill id="cursor" color="#6e0c20" size={15} />
 				</div>
 			</div>
+			<p style={styles.header}>
+				playground | counter strike -{" "}
+				<a style={styles.href} href="http://itaylayzer.github.io/">
+					itay layzer
+				</a>
+				<br />
+				move with A,S,D,W. throw with F
+				<br />
+				shoot with mouse 0. unlock with Escape
+				<br />
+				[fullscreen]: silent movement with Shift. crouch with Ctrl.
+				<br />
+				[numpad]: move camera with 124578.
+				<br />
+				for collision debug mode add "/?debug" in url.
+			</p>
 		</>
 	);
 }
@@ -24,6 +40,22 @@ const styles = useStyles({
 		left: "50%",
 		translate: "-50% -50%",
 		zIndex: 2,
+	},
+	header: {
+		zIndex: 2,
+		color: "white",
+		fontSize: 16,
+		position: "absolute",
+		boxSizing: "border-box",
+		display: "block",
+		bottom: 0,
+		left: "10px",
+		fontFamily: "monospace",
+		textAlign: "left",
+		width: "fit-content",
+	},
+	href: {
+		color: "rgb(41, 131, 255)",
 	},
 });
 
